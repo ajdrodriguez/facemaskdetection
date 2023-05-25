d4ebad909e9c2075b1667ed1210f6468c3e677ae
Kian Jacob Anthony Tubalinal <72308402+Kheijhackt@users.noreply.github.com>
2023-05-25 18:37:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,24 @@ file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
+def import_and_predict(image_path,model):
     size=(35,35)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    image = Image.open(image_path)
+    image=ImageOps.fit(image,size,Image.ANTIALIAS)
     image_rgb = image.convert("RGB")
     img = np.asarray(image_rgb)
     img_reshape=img[np.newaxis,...]
     prediction = model.predict(img_reshape)
     return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
+  
+image_dir = "./For_Testing/"
+image_files = os.listdir(image_dir)  
+  
+selected_image = st.selectbox("Select an image", image_files)
+
+if selected_image:
+    image_path = os.path.join(image_dir, selected_image)
+    st.image(image_path,use_column_width=True)
     prediction=import_and_predict(image,model)
     class_names={0:'MASK INCORRECT',1:'MASK', 2:'NO MASK'}
     string="OUTPUT : "+class_names[np.argmax(prediction)]
